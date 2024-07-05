38664e6480102d1276334397d9560083f7b4491b
nbjorklund <niklas.bjorklund@persgroep.net>
2024-07-05 16:02:39 +0200

    Merge checks for brie and backstage passes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -20,6 +20,12 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
 
+                if (item.sellIn <= 0) {
+                    if (item.quality < MAX_QUALITY) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
             } else if (item.name.equals(BACKSTAGE_PASSES)) {
                 if (item.quality < MAX_QUALITY) {
                     item.quality = item.quality + 1;
@@ -32,23 +38,17 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 }
+
+                if (item.sellIn <= 0) {
+                    item.quality = 0;
+                }
             } else {
                 if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS)) {
                     item.quality = item.quality - 1;
                 }
             }
 
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.sellIn <= 0) {
-                    if (item.quality < MAX_QUALITY) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            } else if (item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.sellIn <= 0) {
-                    item.quality = 0;
-                }
-            } else if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS) && item.sellIn <= 0) {
+            if (item.quality > MIN_QUALITY && item.sellIn <= 0 && !item.name.equals(SULFURAS) && !item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
                 item.quality = item.quality - 1;
             }
 
