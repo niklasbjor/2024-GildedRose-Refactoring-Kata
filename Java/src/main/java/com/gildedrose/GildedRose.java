e9eca3719fc41daa7e15b850d8094aeda6f15da9
nbjorklund <niklas.bjorklund@persgroep.net>
2024-07-05 15:36:29 +0200

    Combine if-statements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,26 +17,20 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals(AGED_BRIE)
                     && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > MIN_QUALITY) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
+                if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS)) {
+                    item.quality = item.quality - 1;
                 }
             } else {
                 if (item.quality < MAX_QUALITY) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 11 && item.quality < MAX_QUALITY) {
+                            item.quality = item.quality + 1;
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < MAX_QUALITY) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
@@ -49,10 +43,8 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (!item.name.equals(AGED_BRIE)) {
                     if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > MIN_QUALITY) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
+                        if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS)) {
+                            item.quality = item.quality - 1;
                         }
                     } else {
                         item.quality = item.quality - item.quality;
