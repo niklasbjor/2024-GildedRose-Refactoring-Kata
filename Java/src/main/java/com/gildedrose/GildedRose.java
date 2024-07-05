32f7857a7dd6e0f60000c761c3f9c57b0a6ced68
nbjorklund <niklas.bjorklund@persgroep.net>
2024-07-05 21:15:31 +0200

    Remove duplication in GildedRose class

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,12 +2,8 @@ package com.gildedrose;
 
 import com.gildedrose.product.Product;
 import com.gildedrose.product.ProductFactory;
-import com.gildedrose.product.RegularItem;
 
 class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     Item[] items;
 
@@ -17,40 +13,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case AGED_BRIE -> {
-                    Product product = ProductFactory.createProduct(item);
-                    updateBrie(product);
-                }
-                case BACKSTAGE_PASSES -> {
-                    Product product = ProductFactory.createProduct(item);
-                    updateBackstagePasses(product);
-                }
-                case SULFURAS -> {
-                    Product product = ProductFactory.createProduct(item);
-                    updateSulfuras(product);
-                }
-                default -> {
-                    Product product = ProductFactory.createProduct(item);
-                    updateRegularItem(product);
-                }
-            }
+            Product product = ProductFactory.createProduct(item);
+            product.update();
         }
     }
 
-    private static void updateRegularItem(Product product) {
-        product.update();
-    }
-
-    private static void updateBrie(Product product) {
-        product.update();
-    }
-
-    private static void updateSulfuras(Product product) {
-        product.update();
-    }
-
-    private static void updateBackstagePasses(Product product) {
-        product.update();
-    }
 }
