2c6a4c0c3723ecc0a41d1c1bffe2920ee455446e
nbjorklund <niklas.bjorklund@persgroep.net>
2024-07-05 15:36:59 +0200

    Simplify calculation

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -47,7 +47,7 @@ class GildedRose {
                             item.quality = item.quality - 1;
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < MAX_QUALITY) {
