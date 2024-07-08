4edc7aeb230a4d25a366888c5242cd481d8e36cc
nbjorklund <niklas.bjorklund@persgroep.net>
2024-07-08 20:23:12 +0200

    Remove unused methods and obsolete javadoc

--- Java/src/main/java/com/gildedrose/domain/Product.java
+++ Java/src/main/java/com/gildedrose/domain/Product.java
@@ -21,10 +21,6 @@ public class Product {
         updateSellIn();
     }
 
-    /**
-     * Do the update of the "quality" property of the item.
-     * To be overridden in each concrete subclass.
-     */
     protected void updateQuality() {
         if (isPastSellByDate()) {
             safelySetQuality(qualityCalculator.apply(item));
@@ -34,24 +30,12 @@ public class Product {
         }
     }
 
-    /**
-     * Do the update of the "sellIn" property of the item.
-     * This default behaviour may be overridden in a concrete subclass.
-     */
     protected void updateSellIn() {
         item.sellIn = item.sellIn - 1;
     }
 
     protected boolean isPastSellByDate() {
         return item.sellIn <= 0;
-    }
-
-    protected void safelyIncreaseQuality(int amount) {
-        item.quality = Math.min(item.quality + amount, MAX_QUALITY);
-    }
-
-    protected void safelyDecreaseQuality(int amount) {
-        item.quality = Math.max(item.quality - amount, MIN_QUALITY);
     }
 
     protected void safelySetQuality(int quality) {
